ce38689997e9154c8a20fe529c721477bbb92707
Diego Costa Alves <61090568+diegobachsi@users.noreply.github.com>
2023-03-07 22:38:29 -0300

    Update settings.py

--- controledeponto/settings.py
+++ controledeponto/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#fq3+t2ogwls@96$6ksquu(a8^ai+aiw80iu5#(pwzri3bini+'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
